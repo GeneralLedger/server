1752bd5298083e34bebafb89b046b4ef3a4a8ce3
BrandonRomano <BrandonRRomano@gmail.com>
2020-02-09 19:22:54 -0800

    Fix relative path issue

--- database/connection.go
+++ database/connection.go
@@ -7,6 +7,8 @@ import (
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"os"
+	"path/filepath"
 )
 
 func MigratedDatabase(databaseURI string) (*sql.DB, error) {
@@ -16,13 +18,21 @@ func MigratedDatabase(databaseURI string) (*sql.DB, error) {
 		return nil, err
 	}
 
+	// Determine directory of Migrations
+	ex, err := os.Executable()
+	if err != nil {
+		return nil, err
+	}
+	exPath := filepath.Dir(ex)
+	migrationsPath := exPath + "/database/migrations"
+
 	// Run our migrations against the database connection
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		return nil, err
 	}
 	dbMigrator, err := migrate.NewWithDatabaseInstance(
-		"file://database/migrations",
+		"file://"+migrationsPath,
 		"postgres",
 		driver,
 	)
