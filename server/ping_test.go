ec188a7967d2f6a590cf84731a3b7e7365c907e3
BrandonRomano <BrandonRRomano@gmail.com>
2020-02-09 19:13:03 -0800

    Add a database to travis

--- server/ping_test.go
+++ server/ping_test.go
@@ -1,16 +1,26 @@
 package server
 
 import (
+	"github.com/generalledger/api/database"
 	"github.com/generalledger/response"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
 func TestPingSuccess(t *testing.T) {
+	// Prepare Test Database
+	db, err := database.MigratedDatabase(os.Getenv("TEST_POSTGRES_URL"))
+	if err != nil {
+		panic(err)
+	}
+
 	// Prepare Server
-	server := &Server{}
+	server := &Server{
+		DB: db,
+	}
 
 	// Send Request
 	recorder := httptest.NewRecorder()
